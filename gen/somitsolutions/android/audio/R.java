bf9048f1bd6925786437f2108f08b8a394149569
Tennyleaz <tennyleazgmail.com>
2014-05-11 20:57:35 +0800

    Added new fragment_sender.xml for sender page.

--- gen/somitsolutions/android/audio/R.java
+++ gen/somitsolutions/android/audio/R.java
@@ -16,13 +16,13 @@ public final class R {
     public static final class id {
         public static final int ascii_input=0x7f050002;
         public static final int binery_output=0x7f050005;
-        public static final int button1=0x7f050003;
         public static final int freq1=0x7f050008;
         public static final int freq2=0x7f050009;
         public static final int freq3=0x7f05000a;
         public static final int input_label_1=0x7f050000;
         public static final int mainLayOut=0x7f050006;
         public static final int output_label_1=0x7f050004;
+        public static final int send_btn=0x7f050003;
         public static final int title1=0x7f050001;
         public static final int tv=0x7f050007;
     }
